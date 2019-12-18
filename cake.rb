9b143dbb5726fa99ff3fd1a21128602bf33d6456
Aseel Alshohatee <aseelalshohatee@Aseels-MBP.home>
2019-12-17 20:15:27 -0500

    adding the price and fix the home page

--- cake.rb
+++ cake.rb
@@ -1,10 +1,11 @@
 class Cake
-  attr_reader :id, :name, :img, :ingredientLines, :price
-  def initialize(id, name, img,ingredientLines, price)
+  attr_reader :id, :name, :img, :ingredientLines, :price, :calories
+  def initialize(id, name, img,ingredientLines, calories, price)
     @id = id
     @name = name
     @img = img
     @ingredientLines = ingredientLines
+    @calories = calories
     @price = price
   end
 end
--- server.rb
+++ server.rb
@@ -6,93 +6,26 @@ set :port, 3000
 require './cake.rb'
 require './muffin.rb'
 require './cookie.rb'
+require 'twilio-ruby'
+
+def h(text)
+  Rack::Utils.escape_html(text)
+end
 
 @@path = "https://api.edamam.com/search"
 
 get '/' do
+  @titlePage = "index"
   erb :index
 end
-
-# here to go to the specific page
-get '/cookie' do
-  @fo ="cookie"
-  @num = 24
-  @id = ENV['API_EDAMAM_ID']
-  @key = ENV['API_EDAMAM_KEY']
-
-  @arr = []
-
-  req = Curl::Easy.perform(@@path + "?q=#{@fo}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
-  res = JSON.parse(req.body)
-
-  i = 0
-  while i < @num
-    @data = res['hits'][i]["recipe"]
-    @ingredientLines = []
-    @data["ingredientLines"].each do |y|
-      @ingredientLines << y
-    end
-
-    @cookie = Cookie.new(i,@data["label"], @data["image"],@ingredientLines, 2 )
-    @arr << @cookie
-    i += 1
-  end
-  erb :cookie
+get '/search' do
+  @titlePage = "search"
+  erb :search
 end
-
-get '/muffin' do
-  @fo ="muffin"
-  @num = 24
-  @id = ENV['API_EDAMAM_ID']
-  @key = ENV['API_EDAMAM_KEY']
-
-  @arr = []
-
-  req = Curl::Easy.perform(@@path + "?q=#{@fo}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
-  res = JSON.parse(req.body)
-
-  i = 0
-  while i < @num
-    @data = res['hits'][i]["recipe"]
-    @ingredientLines = []
-    @data["ingredientLines"].each do |y|
-      @ingredientLines << y
-    end
-
-    @muffin = Muffin.new(i,@data["label"], @data["image"],@ingredientLines, 2 )
-    @arr << @muffin
-    i += 1
+post '/search' do
+  def h(text)
+    Rack::Utils.escape_html(text)
   end
-  erb :muffin
-end
-
-get '/cake' do
-  fo ="cake"
-  @num = 24
-  @id = ENV['API_EDAMAM_ID']
-  @key = ENV['API_EDAMAM_KEY']
-
-  @arr = []
-
-  req = Curl::Easy.perform(@@path + "?q=#{fo}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
-  res = JSON.parse(req.body)
-
-  i = 0
-  while i < @num
-    @data = res['hits'][i]["recipe"]
-    @ingredientLines = []
-    @data["ingredientLines"].each do |y|
-      @ingredientLines << y
-    end
-
-    @cake = Cake.new(i,@data["label"], @data["image"],@ingredientLines, 2 )
-    @arr<< @cake
-    i += 1
-  end
-  erb :cake
-end
-
-post '/' do
   @food = params['food']
   @num = params['to'].to_i
   id = ENV['API_EDAMAM_ID']
@@ -106,5 +39,105 @@ post '/' do
     @arr << @data
     x += 1
   end
-  erb :index
+  erb :cookie
 end
+# here to go to the specific page
+get '/cookie' do
+  @fo ="cookie"
+  @titlePage = "cookie"
+  @num = 24
+  @id = ENV['API_EDAMAM_ID']
+  @key = ENV['API_EDAMAM_KEY']
+
+  @arr = []
+
+  req = Curl::Easy.perform(@@path + "?q=#{@fo}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
+  res = JSON.parse(req.body)
+
+  i = 0
+  while i < @num
+    @data = res['hits'][i]["recipe"]
+    @cal= res['hits'][i]["recipe"]['calories']
+    @ingredientLines = []
+    @data["ingredientLines"].each do |y|
+      @ingredientLines << y
+    end
+
+    @cookie = Cookie.new(i,@data["label"], @data["image"],@ingredientLines, @cal,rand(1.00 .. 4.00) )
+    @arr << @cookie
+    i += 1
+  end
+  erb :cookie
+end
+
+get '/muffin' do
+  @fo ="muffin"
+    @titlePage = "muffin"
+  @num = 24
+  @id = ENV['API_EDAMAM_ID']
+  @key = ENV['API_EDAMAM_KEY']
+
+  @arr = []
+
+  req = Curl::Easy.perform(@@path + "?q=#{@fo}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
+  res = JSON.parse(req.body)
+
+  i = 0
+  while i < @num
+    @data = res['hits'][i]["recipe"]
+    @cal= res['hits'][i]["recipe"]['calories']
+    @ingredientLines = []
+    @data["ingredientLines"].each do |y|
+      @ingredientLines << y
+    end
+
+    @muffin = Muffin.new(i,@data["label"], @data["image"],@ingredientLines,@cal ,rand(1.00 .. 4.00) )
+    @arr << @muffin
+    i += 1
+  end
+  erb :muffin
+end
+
+get '/cake' do
+  fo ="cake"
+  @titlePage = "cake"
+  @num = 24
+  @id = ENV['API_EDAMAM_ID']
+  @key = ENV['API_EDAMAM_KEY']
+
+  @arr = []
+
+  req = Curl::Easy.perform(@@path + "?q=#{fo}&app_id=#{@id}&app_key=#{@key}&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
+  res = JSON.parse(req.body)
+
+  i = 0
+  while i < @num
+    @data = res['hits'][i]["recipe"]
+     @cal= res['hits'][i]["recipe"]['calories']
+    @ingredientLines = []
+    @data["ingredientLines"].each do |y|
+      @ingredientLines << y
+    end
+
+    @cake = Cake.new(i,@data["label"], @data["image"],@ingredientLines, @cal , rand(1.00 .. 4.00) )
+    @arr<< @cake
+    i += 1
+  end
+  erb :cake
+end
+
+# #
+# account_sid = ENV['ACCOUNT_SID_TWILIO']
+#
+# auth_token = ENV['AUTH']
+#
+# @client = Twilio::REST::Client.new(account_sid,auth_token)
+#
+# fromNum = ENV['THE_NUMBER_PRO'] # Your Twilio number
+# toNum= ENV['MY_NUMBER'] # Your mobile phone number
+#
+# @client.api.account.messages.create(
+# from: fromNum,
+# to: toNum,
+# body: "Hey hhhhhhhhhhhh!"
+# )
